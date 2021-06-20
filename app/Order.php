97dc38e148590ad854c43a227da91422b1110800
banu aria <banuaria@gmail.com>
2021-06-20 20:10:28 +0700

    success

--- app/Order.php
+++ app/Order.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $table = 'order';
-    // protected $fillable = ['invoice','user_id','no_resi','status_order_id','ongkir','biaya_cod','subtotal','biaya_cod','pesan','no_hp'];
-    protected $guarded = [];
+    protected $fillable = ['invoice','user_id','no_resi','status_order_id','ongkir','biaya_cod','subtotal','biaya_cod','pesan','no_hp'];
+    // protected $guarded = [];
 }
